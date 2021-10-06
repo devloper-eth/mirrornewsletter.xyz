22cf2e378324c82484edbbcd125d664d189897d5
devloper-eth <89419317+devloper-eth@users.noreply.github.com>
2021-10-06 10:57:08 -0400

    Update fonts

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import SignupForm from "./components/SignupForm.vue";
   <div class="grid grid-cols-1 lg:grid-cols-2 h-full">
     <div class="bg-black">
       <div class="container p-12 xl:p-24 text-white">
-        <a href="/"><img class="h-12 w-12" src="/mirrornewsletter.svg" /></a>
-        <h1 class="mt-16 text-3xl">mirror newsletter</h1>
-        <h2 class="mt-8 text-xl">
+        <a href="/"><img class="h-8 w-8" src="/mirrornewsletter.svg" /></a>
+        <h1 class="mt-16 text-4xl header-font">mirror newsletter</h1>
+        <h2 class="mt-8 text-l paragraph-font">
           mirrornewsletter.xyz is your weekly information source on all things NFT, web3 and DAO. All links curated by
           hand (with top secret ranking algo coming soon).
         </h2>
@@ -16,8 +16,8 @@ import SignupForm from "./components/SignupForm.vue";
       </div>
 
       <div class="pl-12 xl:pl-24 mb-12 mt-12 text-white bg-black">
-        built by
-        <a href="https://twitter.com/devloper_eth">@<span class="underline">devloper_eth</span></a>
+        Follow us on
+        <a href="https://twitter.com/devloper_eth"><span class="underline">twitter</span></a>
       </div>
     </div>
     <div class="hidden lg:block"><!-- empty white space --></div>
