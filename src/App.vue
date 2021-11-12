6a8541e0ccf9b8d795e46c12b85fda39beb2c520
devloper-eth <89419317+devloper-eth@users.noreply.github.com>
2021-11-11 16:17:59 -0800

    Add back issues

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,19 @@ import SignupForm from "./components/SignupForm.vue";
           hand (with top secret ranking algo coming soon).
         </h2>
         <SignupForm class="mt-12" />
-      </div>
+        <h1 class="mt-8 text-sm paragraph-font">
+          <span class="pr-2">
+            Past Issues
+          </span>
+          <span class="pr-2 text-sm paragraph-font">
+            <a class="hover:underline" href="https://mirrornewsletter.xyz/issue/1">#1</a>,
+            <a class="hover:underline" href="https://mirrornewsletter.xyz/issue/2">#2</a>,
+            <a class="hover:underline" href="https://mirrornewsletter.xyz/issue/3">#3</a>
+          </span>
+        </h1>
 
-      <div class="pl-12 xl:pl-24 mb-12 mt-12 text-white bg-black">
+      </div>
+      <div class="pl-12 xl:pl-24 mb-12 text-white bg-black">
         Follow us on
         <a href="https://twitter.com/devloper_eth"><span class="underline">twitter</span></a>
       </div>
